c3fa6e83036d664444a3a8ff542d020091bdd218
Cédric Degrelle <cedric.degrelle-dev@hotmail.com>
2019-12-07 12:02:03 +0100

    add header and style product-list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,9 @@ import ProductList from '../containers/ProductList'
 const App = () => {
     return (
         <div className="app">
+            <header className="app__header">
+                <h1>Vending Machine</h1>
+            </header>
             <main className="app__main">
                 <ProductList />
             </main>
