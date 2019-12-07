31e1e3cb71de13deab2f68eaee0ad1c5447d6bc6
Cédric Degrelle <cedric.degrelle-dev@hotmail.com>
2019-12-07 13:09:37 +0100

    use selecteProduct action in product-list-item

--- src/components/ProductListItem.jsx
+++ src/components/ProductListItem.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const ProductListItem = ({ product }) => {
+const ProductListItem = ({ product, selectedIndex, onSelectProduct }) => {
+    const disabled = product.stock <= 0 || (selectedIndex && selectedIndex !== product.id)
+
     return (
         <div className="product-list-item">
             <h4>{product.name}</h4>
@@ -9,7 +11,7 @@ const ProductListItem = ({ product }) => {
                 <div>Price: <strong>£{product.price.toFixed(2)}</strong></div>
                 <div>Stock: <strong>{product.stock}</strong></div>
             </div>
-            <button disabled={product.stock <= 0} className="product-list-item__select">Select</button>
+            <button disabled={disabled} onClick={() => onSelectProduct(product.id)} className="product-list-item__select">Select</button>
         </div>
     )
 }
@@ -20,7 +22,9 @@ ProductListItem.propTypes = {
         name: PropTypes.string.isRequired,
         price: PropTypes.number.isRequired,
         stock: PropTypes.number.isRequired,
-    })
+    }),
+    selectedIndex: PropTypes.number,
+    onSelectProduct: PropTypes.func.isRequired
 }
 
 export default ProductListItem
--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -3,23 +3,31 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
 import ProductListItem from '../components/ProductListItem'
+import { selectProduct } from '../actions'
 
-const ProductList = ({ products }) => {
+const ProductList = ({ products, selectedIndex, handleSelectProduct }) => {
     return (
         <div className="product-list">
             {products.map(product => (
-                <ProductListItem key={product.id} product={product} />
+                <ProductListItem key={product.id} product={product} selectedIndex={selectedIndex} onSelectProduct={handleSelectProduct} />
             ))}
         </div>
     )
 }
 
 ProductList.propTypes = {
-    products: PropTypes.arrayOf(PropTypes.object).isRequired
+    products: PropTypes.arrayOf(PropTypes.object).isRequired,
+    selectedIndex: PropTypes.number,
+    handleSelectProduct: PropTypes.func.isRequired
 }
 
 const mapStateToProps = state => ({
-    products: state.productReducer.products
+    products: state.productReducer.products,
+    selectedIndex: state.productReducer.selectedIndex
 })
 
-export default connect(mapStateToProps)(ProductList)
+const mapDispatchToProps = dispatch => ({
+    handleSelectProduct: id => dispatch(selectProduct(id))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductList)
