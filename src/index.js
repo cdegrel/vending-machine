d5bacf2bfa770e71ffbda305df9f942f25720c48
Cédric Degrelle <cedric.degrelle-dev@hotmail.com>
2019-12-07 11:45:18 +0100

    configure redux store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
 
+import { Provider } from 'react-redux'
+import store from './store'
+
 import App from './components/App'
 
 import './style.css'
 
 render(
-    <App/>,
+    <Provider store={store}>
+        <App/>
+    </Provider>,
     document.querySelector('#root')
 )
