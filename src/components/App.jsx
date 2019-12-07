4a8a0798c739edd5849911c16941be65bc5b2a2d
Cédric Degrelle <cedric.degrelle-dev@hotmail.com>
2019-12-07 23:44:32 +0100

    add route path

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,19 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 
 import Header from './Header'
-
-import ProductList from '../containers/ProductList'
-import ControllerPanel from '../containers/ControllerPanel'
-import ProductPurchasedList from '../containers/ProductPurchasedList'
+import Machine from './Machine'
+import Admin from './Admin'
 
 const App = () => {
     return (
         <div className="app">
             <Header />
             <main className="app__main">
-                <div className="vending-machine">
-                    <ProductList />
-                    <ControllerPanel />
-                </div>
-                <ProductPurchasedList />
+                <Switch>
+                    <Route exact path="/" component={Machine}/>
+                    <Route path="/admin" component={Admin}/>
+                </Switch>
             </main>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
-
 import { Provider } from 'react-redux'
-import store from './store'
+import { BrowserRouter as Router } from 'react-router-dom'
 
+import store from './store'
 import App from './components/App'
 import { fetchCoins, fetchProducts } from './actions'
 
@@ -14,7 +14,9 @@ store.dispatch(fetchCoins())
 
 render(
     <Provider store={store}>
-        <App/>
+        <Router>
+            <App/>
+        </Router>
     </Provider>,
     document.querySelector('#root')
 )
