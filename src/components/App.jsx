7a3a5abd118bba3187e9f18a47bb4092ed307b15
Cédric Degrelle <cedric.degrelle-dev@hotmail.com>
2019-12-07 23:12:48 +0100

    create header component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Header from './Header'
+
 import ProductList from '../containers/ProductList'
 import ControllerPanel from '../containers/ControllerPanel'
 import ProductPurchasedList from '../containers/ProductPurchasedList'
@@ -7,9 +9,7 @@ import ProductPurchasedList from '../containers/ProductPurchasedList'
 const App = () => {
     return (
         <div className="app">
-            <header className="app__header">
-                <h1>Vending Machine</h1>
-            </header>
+            <Header />
             <main className="app__main">
                 <div className="vending-machine">
                     <ProductList />
