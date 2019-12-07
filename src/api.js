12e83fead97df86fe8550a60443450e57a0ca474
Cédric Degrelle <cedric.degrelle-dev@hotmail.com>
2019-12-07 11:38:58 +0100

    add product data

---
+++ src/api.js
@@ -0,0 +1,38 @@
+export const PRODUCS = [
+    {
+        id: 1,
+        name: "Coca Cola",
+        stock: 4,
+        price: 1.1
+    },
+    {
+        id: 2,
+        name: "Orangina",
+        stock: 5,
+        price: 1.5
+    },
+    {
+        id: 3,
+        name: "Fanta",
+        stock: 2,
+        price: 2
+    },
+    {
+        id: 4,
+        name: "Pepsie",
+        stock: 1,
+        price: 1.3
+    },
+    {
+        id: 5,
+        name: "7 Up",
+        stock: 4,
+        price: 0.8
+    },
+    {
+        id: 6,
+        name: "Ice Tea",
+        stock: 3,
+        price: 1.6
+    },
+]
